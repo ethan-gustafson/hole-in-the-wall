d5cb6146c0fae3d7e606e12216b7ac4c76f75d64
Ethan <ethan_gustafson@outlook.com>
2020-01-18 23:54:36 -0500

    added basic styling and structure to StoresController

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,12 +1,35 @@
 class StoresController < ApplicationController
 
     get '/stores' do
-        erb :'/stores/show_stores'
+        if logged_in? # session works, users logged in will see 
+        erb :'/stores/show_stores' # all of the stores.
+        else
+            redirect to '/hole-in-the-wall'
+        end
+    end
+
+    get '/stores/:id' do
+        if logged_in?
+            
+            else
+                redirect to '/hole-in-the-wall'
+            end
     end
 
     get '/my-stores' do
-
+        if logged_in?
+            
+            else
+                redirect to '/hole-in-the-wall'
+            end
     end
 
+    get '/my-stores/:id' do
+        if logged_in?
+            
+            else
+                redirect to '/hole-in-the-wall'
+            end
+    end
 
 end
