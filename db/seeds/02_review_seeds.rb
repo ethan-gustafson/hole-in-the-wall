b2a9a3a00133fa6e79493bebd38da89b9c0dd18f
Ethan <ethan_gustafson@outlook.com>
2020-01-20 00:08:43 -0500

    fixed remaining review seeds

--- db/seeds/02_review_seeds.rb
+++ db/seeds/02_review_seeds.rb
@@ -1,61 +1,61 @@
 # 1
-saigon_bistro = Review.create(:title => "High quality food! - Saigon Bistro", 
+saigon_bistro = Review.create(:title => "High quality food!", 
 :content => "Saigon Bistro is a vietnamese hole in the wall on the edge of Daytona Beach and Ormond Beach. It is one of the best gems of the area with amazing food and lovely service. It's too bad the place doesn't get many visitors. They feature Pho, Hu Tieu and Mi, rice plates, egg noodles, Rice Vermicelli, Banh Mi, and other Vietnamese classics. Saigon Bistro gives amazing food for a low price.", 
 :user_id => 1, 
 :store_id => 1
 )
 
 # 2
-saigon_bistro_2 = Review.create(:title => "Something New - Saigon Bistro",
+saigon_bistro_2 = Review.create(:title => "Something New",
 :content => "Let's start off by saying that it should be illegal to make food this good. Jk, but it's truly amazing. I've been to another honey bake out west, and this one hands down wins. The food seems to be served with more love. The staff is amazing. Also, a bunch of discounts, and military friendly.",
 :user_id => 2,
 :store_id => 1
 )
 
 # 3
-saigon_bistro_3 = Review.create(:title => "Lovely Service - Saigon Bistro",
+saigon_bistro_3 = Review.create(:title => "Lovely Service",
 :content => "They gave me a bowl of Pho - and I've never had such a euphoric moment in my life.",
 :user_id => 3,
 :store_id => 1
 )
 
 # 4
-hot_dog_heaven = Review.create(:title => "This Really is Heaven - Hot Dog Heaven",
+hot_dog_heaven = Review.create(:title => "This Really is Heaven",
 :content => "Second time here, but 1st time in awhile. Great hot dogs with various toppings. We ate lunch there and during the week no less than 30 people there. They take credit cards for purchases over $5.00. I rarely, rarely, hand out 5 ratings, but for a hot dog, this can't be beat.",
 :user_id => 1,
 :store_id => 2
 )
 
 # 5
-hot_dog_heaven_2 = Review.create(:title => "Long line: Worth it - Hot Dog Heaven",
+hot_dog_heaven_2 = Review.create(:title => "Long line: Worth it",
 :content => "I hesitate to write this review because they are now closed and it’s going to make me hungry for them again. The Vienna beef franks and the poppy seed buns are always perfect. And the staff is always on point. Seriously, one of the hardest working crews of any joint in the city. Ways a great visit.",
 :user_id => 2,
 :store_id => 2
 )
 
 # 6
-hot_dog_heaven_3 = Review.create(:title => "What kind of food is this? - Hot Dog Heaven",
+hot_dog_heaven_3 = Review.create(:title => "What kind of food is this?",
 :content => "I keep hearing people say that Hot Dogs are served here. They better not be hot 'Dogs'!! My brethren are not food. Watched my humans eat their hot 'dogs' in disgust.",
 :user_id => 3,
 :store_id => 2
 )
 
 # 7
-mannys_pizza_house = Review.create(:title => "My Favorite Pizza Place - Manny's Pizza House ",
+mannys_pizza_house = Review.create(:title => "My Favorite Pizza Place",
 :content => "it will always be my favorite pizza house. The food is delicious and everyone there is friendly and appreciative for your business. It’s location is quiet which makes it that much more enjoyable. 10 out of 10!!!",
 :user_id => 1,
 :store_id => 3
 )
 
 # 8
-mannys_pizza_house_2 = Review.create(:title => "Wow! - Manny's Pizza House",
+mannys_pizza_house_2 = Review.create(:title => "Wow!",
 :content => "We always stop here whenever we are in the area. I have NEVER had a meal that wasn’t excellent. You get large portions and the price is very reasonable. Always fresh ingredients in everything. Family owned and operated. Great time every time!",
 :user_id => 2,
 :store_id => 3
 )
 
 # 9
-mannys_pizza_house_3 = Review.create(:title => "Best Pizza I have ever had - Manny's Pizza House",
+mannys_pizza_house_3 = Review.create(:title => "Best Pizza I have ever had!",
 :content => "To be fair I am a dog and I have not been given the opportunity to eat pizza. First time. This food is dank.",
 :user_id => 3,
 :store_id => 3
