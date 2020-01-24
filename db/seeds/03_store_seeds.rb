f6476c2757f73fe784f1207b5e04b887a40384a5
Ethan <ethan_gustafson@outlook.com>
2020-01-23 21:13:37 -0500

    changed store images path

--- db/seeds/03_store_seeds.rb
+++ db/seeds/03_store_seeds.rb
@@ -3,7 +3,7 @@ saigon_bistro = Store.create(:name => "Saigon Bistro",
 :address => "600 S Yonge St # 16B, Ormond Beach, FL 32174", 
 :url => "http://www.saigon-bistro.com/index.html", 
 :description => "A Vietnamese Restaurant offers a naturally healthy option. There are lots of delicious vegetables in with the meat and spices. The spices used are different enough that they are exciting alternatives to most Americans' dinners. A Vietnamese Restaurant is a great choice if you're looking for something new and exciting.",
-:image => "https://www.news-journalonline.com/storyimage/LK/20150505/News/605056191/V3/0/V3-605056191.jpg"
+:image => "/images/Saigon-Bistro.jpg"
 )
 
 # 2
@@ -11,7 +11,7 @@ hot_dog_heaven = Store.create(:name => "Hot Dog Heaven",
 :address => "5355 E Colonial Dr, Orlando, FL 32807", 
 :url => "https://www.tripadvisor.com/Restaurant_Review-g34515-d808843-Reviews-Hot_Dog_Heaven-Orlando_Florida.html", 
 :description => "Authentic Chicago Hot Dogs",
-:image => "https://i.pinimg.com/originals/7b/78/a0/7b78a08c9f4ca2dd86eb03128b353fce.jpg"
+:image => "/images/Hot-Dog-Heaven.jpg"
 )
 
 # 3
@@ -19,7 +19,7 @@ mannys_pizza_house = Store.create(:name => "Manny's Pizza House",
 :address => "487 S Nova Rd Ormond Beach, FL 32174", 
 :url => "https://www.mannyspizzahouse.com/", 
 :description => "Visit Manny's Pizza House for authentic pizzas, appetizers, Italian cuisine, and much more.",
-:image => "https://media-cdn.tripadvisor.com/media/photo-s/08/bd/8f/0b/manny-s-pizza-house.jpg"
+:image => "/images/pizza.jpg"
 )
 
 # 4
@@ -27,7 +27,7 @@ drunken_monkey_coffee_bar = Store.create(:name => "Drunken Monkey Coffee Bar",
 :address => "444 N Bumby Ave, Orlando FL 32803", 
 :url => "http://drunkenmonkeycoffee.com/", 
 :description => "Drunken Monkey Coffee Bar was conceived to fill the need in Orlando for excellent coffee and food that everyone can eat... think college-town coffeehouse.",
-:image => "https://cdn.usarestaurants.info/assets/uploads/041700071723995a0012777541533802_-united-states-florida-orange-county-orlando-drunken-monkey-coffee-bar-407-893-4994htm.jpg"
+:image => "/images/Drunken-monkey.jpg"
 )
 
 # 5
@@ -35,7 +35,7 @@ beefy_king = Store.create(:name => "Beefy King",
 :address => "424 N Bumby Ave, Orlando, FL 32803", 
 :url => "http://beefyking.com/", 
 :description => "Serving the BEST Roast Beef sandwich for over 40 years! Come into Beefy King today and try one of our award winning products. Offering a variety of mouthwatering Roast Beef, Ham, Turkey, Pastrami, Corned Beef, Bar BQ Beef and Bar BQ Pork all served hot, fresh and made to order!",
-:image => "https://www.chicagotribune.com/resizer/ml5F7YbpH5fm-L2VsQslRNtF76A=/fit-in/800x533/smart/filters:fill(black)/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/IPM63HDMMRFLBNYTXYPQDCLMIU.jpg"
+:image => "/images/beefy-king.jpg"
 )
 
 # 6
@@ -43,5 +43,5 @@ hollerbachs_willow_tree_cafe = Store.create(:name => "Hollerbach's Willow Tree C
 :address => "205 E 1st St, Sanford, FL 32771", 
 :url => "https://www.hollerbachs.com/", 
 :description => "Bright, friendly spot offering schnitzel, wurst & other German classics, plus beer & live music.",
-:image => "https://www.mbca.org/sites/default/files/styles/header_image_half/public/event_notice/IMG_20180706_202138.jpg?itok=M43XeG12"
+:image => "/images/hollerbachs-inside-store.jpg"
 )
