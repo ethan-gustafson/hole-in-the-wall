a4e8cccebbfad7dcdab7b4fd8372ff47717dcc2e
Ethan <ethan_gustafson@outlook.com>
2020-05-01 03:21:42 -0400

    fixed issue with deleted posts that weren't actually deleting

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,26 @@
 class ReviewsController < ApplicationController
 
-    get "/my-reviews" do
+    get ("/my-reviews") {
         redirect_if_not_logged_in?
         @reviews = current_user.reviews # shows all of this specific user's reviews
-        erb :'/reviews/show' # Gives links to each review - Gives link to the Home Page
-    end
+        erb :'/reviews/show_my_reviews' # Gives links to each review - Gives link to the Home Page
+    }
 
     get "/my-reviews/:id" do
         redirect_if_not_logged_in?
         validreview? # if the user is logged in and their user_id is equal to the @user_review params id
         @user_review = Review.find_by_id(params[:id]) # finds the right review from post "/my-reviews" @review
-        erb :'/reviews/show' # Shows the correct review.
+        if @user_review
+            erb :'/reviews/show' # Shows the correct review.
+        else
+            redirect to "/my-reviews"
+        end
     end
 
     post "/my-reviews/:id" do
         @user_review = Review.new(params[:review])
         if @user_review.valid?
-            @user_review.store_id = params[:id]
+            @user_review.store_id = params[:store_id]
             @user_review.user_id = session[:user_id]
             @user_review.save
             redirect to "/my-reviews/#{@user_review.id}" # it is redirected to that specific review from @review
@@ -48,7 +52,7 @@ class ReviewsController < ApplicationController
         @user_review = Review.find_by_id(params[:id]) # will find the review by the params id
 
         validreview?  # if the current user is equal to the review user id
-        @user_review.delete # then we will delete the post. 
+        @user_review.destroy # then we will delete the post. 
         redirect '/my-reviews' # Redirected to their reviews.
     end
 
