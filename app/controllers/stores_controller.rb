4e7c5cbe9c2f6c3162ef5f7f24dd35629ff04c94
Ethan <ethan_gustafson@outlook.com>
2020-09-03 00:51:37 -0400

    namespaced stores_controller, added edit/update routes

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -2,29 +2,50 @@ class StoresController < ApplicationController
 
     before { redirect_if_not_logged_in? }
 
-    get '/stores' do # index
-        loggedin_banner
-        erb :'/stores/index'
-    end
+    namespace "/stores" do
 
-    get '/stores/:id' do # show
-        loggedin_banner_dynamic
-        api_k
-        
-        @store = Store.find_by_id(params[:id])
-        erb :'/stores/show'
-    end
-
-    get '/store/new' do # new
-        loggedin_banner_dynamic
-        erb :'/stores/new'
-    end
-
-    post '/stores' do # create
-        @store = Store.new(params)
-        if @store.save
-            redirect "/stores/#{@store.id}"
+        get "/" do # stores#index == get "/stores"
+            loggedin_banner
+            erb :'/stores/index'
         end
+
+        get "/new" do # stores#new == get "/stores/new"
+            loggedin_banner_dynamic
+            erb :'/stores/new'
+        end
+    
+        get "/:id" do # stores#show == get "/stores/:id"
+            loggedin_banner_dynamic
+            api_k
+            
+            @store = Store.find_by_id(params[:id])
+            erb :'/stores/show'
+        end
+    
+        post "/" do # stores#create == post "/stores"
+            @store = Store.new(params)
+            if @store.save
+                redirect "/stores/#{@store.id}"
+            end
+        end
+
+        get "/:id/edit" do # stores#edit == get "/stores/:id/edit"
+            loggedin_banner_dynamic
+            api_k
+            
+            @store = Store.find_by_id(params[:id])
+            erb :'/stores/edit'
+        end
+
+        patch "/:id" do # stores#update == patch "/stores/:id"
+            @store = Store.find_by_id(params[:id]) 
+            if @store.update(params)
+                redirect "/stores/#{@store.id}"
+            else
+                redirect "/stores/#{@store.id}"
+            end
+        end
+        
     end
 
     # get "/search-stores" do
