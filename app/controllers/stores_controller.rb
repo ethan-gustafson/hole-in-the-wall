6aa2455c9bf39773bcfcee8855b855cf48c1fb91
Ethan <ethan_gustafson@outlook.com>
2020-09-04 03:09:57 -0400

    adjusted helpers, added locals, store_params & set_store method in stores_controller

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,42 +1,61 @@
 class StoresController < ApplicationController
 
-    before { redirect_if_not_logged_in? }
+    before { redirect_outside? }
 
     get "/stores" do # stores#index == get "/stores"
         @stores = Store.all
-        erb :'/stores/index'
+        erb :'/stores/index', locals: {title: "Store's index"}
     end
 
     post "/stores" do # stores#create == post "/stores"
-        @store = Store.new(params)
-        if @store.save
+        if  @store = Store.create(store_params)
             redirect "/stores/#{@store.id}"
+        else
+            redirect "/stores/new"
         end
     end
 
     get "/stores/new" do # stores#new == get "/stores/new"
-        erb :'/stores/new'
+        erb :'/stores/new', locals: {title: "Create a Store"}
     end
 
     get "/stores/:id" do # stores#show == get "/stores/:id"
-        
-        @store = Store.find_by_id(params[:id])
-        erb :'/stores/show'
+        set_store
+        erb :'/stores/show', locals: {title: "#{@store.name}"}
     end
 
     get "/stores/:id/edit" do # stores#edit == get "/stores/:id/edit"
-        
-        @store = Store.find_by_id(params[:id])
-        erb :'/stores/edit'
+        set_store
+        erb :'/stores/edit', locals: {title: "Edit: #{@store.name}"}
     end
 
     patch "/stores/:id" do # stores#update == patch "/stores/:id"
-        @store = Store.find_by_id(params[:id]) 
-        if @store.update(params)
+        set_store
+        if @store.update(store_params)
             redirect "/stores/#{@store.id}"
         else
             redirect "/stores/#{@store.id}"
         end
+    end
+
+    private
+
+    def store_params
+        key = require_param(:store)
+    
+        hash = permit_params(
+            key,
+            :name, 
+            :address,
+            :description,
+            :website
+        )
+        hash[:store].store(:user_id, params[:store][:user_id]) if params[:store][:user_id]
+        hash[:store]
+    end
+
+    def set_store
+        @store = Store.find_by_id(params[:id]) 
     end
 
 end
