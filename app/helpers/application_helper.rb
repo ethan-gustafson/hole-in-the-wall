13f7b616d3a1514df09eaac9ab42d0ff0ae7cd5b
Ethan <ethan_gustafson@outlook.com>
2020-09-06 01:33:33 -0400

    fixed states method issue where it was returning an array instead of a hash, adjusted stores#search erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,23 +6,24 @@ module ApplicationHelper
     # The resulting @states instance variable will now hold a hash of key/value pairs of {state: abbreviation}. This method is
     # only set to run once, when the application is initiated, so that it won't set the @states instance variable again.
 
-    def states
-        if @states.nil?
-            filepath                = File.join(".", "app", "assets", "files", "states.txt")
-            file                    = File.read(filepath)
-            states_array            = file.split("\n")
+    def get_states
+        filepath                = File.join(".", "app", "assets", "files", "states.txt")
+        file                    = File.read(filepath)
+        states_array            = file.split("\n")
 
-            states = {}
+        states = {}
 
-            states_array.each do |state|
-                key                = state.slice(0, state.index("-") - 1)
-                value              = state.slice((state.length - 2), state.length)
-                states[key.to_sym] = value
-                @states = states
-            end
-        else
-            @states
+        states_array.each do |state|
+            key                = state.slice(0, state.index("-") - 1)
+            value              = state.slice((state.length - 2), state.length)
+            states[key.to_sym] = value
+            @states = states
         end
+        @states
+    end
+
+    def states
+        @states ||= get_states
     end
 
     # Google will send you emails about your exposed API key for google maps on github.
