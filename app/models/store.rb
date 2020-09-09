0d1d03e2cc332b72bb7d48e09e131594e4d322c9
Ethan <ethan_gustafson@outlook.com>
2020-09-09 05:13:51 -0400

    users can fully edit stores

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,6 +1,5 @@
 class Store < ActiveRecord::Base
-    validates :name, :street, :city, :state, :zip_code, :description, :website, presence: true
-    validates :user_id, presence: true, on: :create
+    validates :name, :street, :city, :state, :zip_code, :description, :website, :user_id, presence: true
 
     # Zip codes in the United States are exactly 5 digits in length
     validates :zip_code, length: { is: 5 }
