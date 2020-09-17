f00494723596f55e8e65277892b681e7ea3d5cfe
Ethan <ethan_gustafson@outlook.com>
2020-09-17 17:35:09 -0400

    fixed form spacing on signup form, sessions_controller refactored root

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,14 @@
 class SessionsController < ApplicationController
 
-    get "/login" do  # sessions#new
+    get "/login" do
         redirect_inside?
         erb :'/sessions/login'
     end
 
-    post "/" do # sessions#create
-        # First find the user from the params
+    post "/" do
         @user = User.find_by(username: params[:user][:username])
-        # if there is a user and the password does authenticate with the user
-        if !!@user && !!@user.authenticate(params[:user][:password]) 
-            # create a new session with a key of user_id pointing to the value of the logged in user id
+
+        if !@user.nil? && !!@user.authenticate(params[:user][:password]) 
             session[:user_id] = @user.id 
             redirect '/'
         else
@@ -20,16 +18,13 @@ class SessionsController < ApplicationController
         end
     end
 
-    get "/" do # root#home
+    get "/" do
         redirect_outside?
-        # reviews_count will always be 5 less than the count of all reviews
-        reviews_count = Review.all.count - 5 
-        # @home_feed_reviews will only show ten reviews from the most recent
-        @home_feed_reviews = Review.all[reviews_count..Review.all.count].reverse 
+        @home_feed_reviews = Review.last(5).reverse 
         erb :'/sessions/root'
     end
 
-    get "/logout" do # sessions#destroy
+    get "/logout" do
 		session.clear
 		redirect "/login"
     end
