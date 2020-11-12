5e9d887fb772b9fa02521abdf68edf98b76d470f
Ethan <ethan_gustafson@outlook.com>
2020-11-11 21:08:28 -0500

    add requires for sinatra base, flash - time and pry

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,12 @@
 # The Bundler runtime allows its two main methods, Bundler.setup and Bundler.require, 
 # to limit their impact to particular groups.
 require 'bundler/setup'
-Bundler.require
 # .require requires all of the gems in the specified groups.
+Bundler.require
+
+require 'sinatra/base'
+require 'sinatra/flash'
+require 'time'
+require 'pry'
 
 require_all 'app'
