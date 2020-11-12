aff2788c54f8607e92f9bf96c920e7273fcafd35
Ethan <ethan_gustafson@outlook.com>
2020-11-12 12:37:07 -0500

    change indentation, add strong params methods, place search conditions into helper, controller functional

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,99 +1,89 @@
 class StoresController < ApplicationController
+  before { redirect_logged_out_user_to_login? }
 
-    before { redirect_outside? }
+  get "/stores" do # stores#index
+    @popular_stores = Store.popular_stores
+    @most_reviewed_stores = Store.most_reviewed_stores
+    @state = ""
+    @stores_count = Store.count
+    erb :'/stores/main'
+  end
 
-    get "/stores" do # stores#main
-        popular_stores
-        most_reviewed_stores
-        
-        @state ||= ""
-        @stores_count = Store.all.count
-        erb :'/stores/main'
+  get "/stores/new" do # stores#new
+    erb :'/stores/new' 
+  end
+
+  post "/stores" do # stores#create
+    @store = new_strong_params(
+      params,
+      Store,
+      :name,
+      :street,
+      :city,
+      :state,
+      :zip_code,
+      :description,
+      :website,
+      :user_id
+    )
+    @store.user_id = current_user.id
+    if @store.save
+      redirect_to store_path(@store.id)
+    else
+      redirect_to new_store_path
     end
+  end
 
-    get ("/stores/new"){ erb :'/stores/new' } # stores#new
+  post "/stores/results" do # stores#create-search-results
+    search_conditions(params)
+    redirect_to search_results_path
+  end
 
-     post "/stores" do # stores#create
-        @store = Store.new(
-            name: params[:store][:name],
-            street: params[:store][:street],
-            city: params[:store][:city],
-            state: params[:store][:state],
-            zip_code: params[:store][:zip_code],
-            description: params[:store][:description],
-            website: params[:store][:website],
-            user_id: current_user.id
-        )
+  get '/stores/search-results' do # stores#search-results
+    @results = 
+      if flash[:search_results]
+        flash[:search_results]
+      else
+        []
+      end
+    erb :'/stores/results'
+  end
 
-        if @store.save
-            redirect "/stores/#{@store.id}"
-        else
-            redirect "/stores/new"
-        end
+  get "/stores/:id" do # stores#show
+    set_store
+    @favorited = Favorite.find_by_user_id_and_store_id(current_user.id, params[:id])
+    erb :'/stores/show'
+  end
+
+  get "/stores/:id/edit" do # stores#edit
+    invalid_resource?
+    erb :'/stores/edit'
+  end
+
+  patch "/stores/:id" do # stores#update
+    invalid_resource?
+    if update_strong_params(
+      params, 
+      Store, 
+      @store, 
+      :name, 
+      :street, 
+      :city, 
+      :state, 
+      :zip_code,
+      :description,
+      :website
+    )
+      redirect_to store_path(@store.id)
+    else
+      redirect_to store_path(@store.id)
     end
+  end
 
-    post "/stores/results" do # stores#create-search-results
-        popular_stores
-        most_reviewed_stores
-        
-        if !params[:state].blank? && !params[:name].blank?
-            if states.has_key?(params[:state].to_sym) 
-                flash[:search_results] = stores_search(store_names_by_state(params[:state]), params[:name])
-            else
-                flash[:search_results] = ["No results found"]
-            end
+  private
 
-        elsif !params[:state].blank? && params[:name].blank?
-
-            if states.has_key?(params[:state].to_sym) 
-                flash[:search_results] = store_names_by_state(params[:state])
-            else
-                flash[:search_results] = ["No results found"]
-            end
-
-        elsif params[:state].blank? && !params[:name].blank?
-            if !store_names(params[:name]).empty?
-                flash[:search_results] = @search_results
-            else
-                flash[:search_results] = ["No results found"]
-            end
-        else
-            flash[:search_results] = Store.select(:id, :name, :state).order(state: :asc).as_json
-        end
-        redirect '/stores/search-results'
-    end
-
-    get '/stores/search-results' do # stores#search-results
-        !!flash[:search_results] ? @results = flash[:search_results] : @results = []
-        erb :'/stores/results'
-    end
-
-    get "/stores/:id" do # stores#show
-        set_store
-        @favorited = Favorite.find_by_user_id_and_store_id(current_user.id, params[:id])
-        erb :'/stores/show'
-    end
-
-    get "/stores/:id/edit" do # stores#edit
-        invalid_resource?
-        erb :'/stores/edit'
-    end
-
-    patch "/stores/:id" do # stores#update
-        invalid_resource?
-
-        if @store.update(
-            name: params[:store][:name],
-            street: params[:store][:street],
-            city: params[:store][:city],
-            state: params[:store][:state],
-            zip_code: params[:store][:zip_code],
-            description: params[:store][:description],
-            website: params[:store][:website]
-        )
-            redirect "/stores/#{@store.id}"
-        else
-            redirect "/stores/#{@store.id}"
-        end
-    end
+  def set_store
+    @store = Store.find_by_id(params[:id]) 
+    redirect_to stores_path if @store.nil?
+  end
 end
