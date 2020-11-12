ecba0c6c7f9b72d3218d2ce0cb4050455d4a3faa
Ethan <ethan_gustafson@outlook.com>
2020-11-12 12:40:24 -0500

    favorites controller functional, add path helper methods, change indentation

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,33 +1,27 @@
 class FavoritesController < ApplicationController
-
-    post '/favorites' do # favorites#create
-        already_favorited?
-
-        favorited_store = Favorite.new(
-            user_id: current_user.id, 
-            store_id: params[:store_id]
-        )
-        if favorited_store.save 
-            redirect "/stores/#{favorited_store.store_id}"
-        else
-            flash[:favorite_error] = "There was an error in favoriting this store."
-            redirect "/stores/#{params[:store_id]}"
-        end
+  post '/favorites' do # favorites#create
+    already_favorited?
+    favorited_store = Favorite.new(user_id: current_user.id, store_id: params[:store_id])
+    if favorited_store.save 
+      redirect_to store_path(favorited_store.store_id)
+    else
+      flash[:favorite_error] = "There was an error in favoriting this store."
+      redirect_to store_path(params[:store_id])
     end
+  end
 
-    get '/favorites/:id/delete' do # favorites#destroy
-        favorite = Favorite.find_by_id(params[:id]) 
-        favorite.delete
-        redirect "/users/#{current_user.id}"
+  get '/favorites/:id/delete' do # favorites#destroy
+    favorite = Favorite.find_by_id(params[:id]) 
+    favorite.delete
+    redirect_to user_path(current_user.id)
+  end
+
+  private
+
+  def already_favorited?
+    if !Favorite.find_by_user_id_and_store_id(current_user.id, params[:store_id]).nil?
+      flash[:favorite_error] = "This store is already in your favorites"
+      redirect_to store_path(params[:store_id])
     end
-
-    private
-
-    def already_favorited?
-        if !Favorite.find_by_user_id_and_store_id(current_user.id, params[:store_id]).nil?
-            flash[:favorite_error] = "This store is already in your favorites"
-            redirect "/stores/#{params[:store_id]}"
-        end
-    end
-
+  end
 end
