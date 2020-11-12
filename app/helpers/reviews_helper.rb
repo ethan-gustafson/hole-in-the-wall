df0472d685854d76c1fc7e06f64cb02a79036d72
Ethan <ethan_gustafson@outlook.com>
2020-11-12 11:49:50 -0500

    add review path helper, set_review & review belongs to user condition

---
+++ app/helpers/reviews_helper.rb
@@ -0,0 +1,13 @@
+module ReviewsHelper
+  def edit_review_path(id)
+    "/reviews/#{id}/edit"
+  end
+  
+  def set_review
+    @review = Review.find_by_id(params[:id]) 
+  end
+
+  def review_belongs_to_user
+    @review.user_id.equal?(current_user.id)
+  end
+end
