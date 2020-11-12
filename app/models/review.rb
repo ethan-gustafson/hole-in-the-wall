32442c5ac4a1a389bbcf7948bcd22952dfc3574e
Ethan <ethan_gustafson@outlook.com>
2020-11-12 11:53:26 -0500

    change indentation, add class method last five reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,12 @@
 class Review < ActiveRecord::Base
-    belongs_to :user
-    belongs_to :store
-    
-    validates :title, :content, presence: true
-    validates :user_id, :store_id, presence: true, on: :create
-    validates :title, uniqueness: true
+  belongs_to :user
+  belongs_to :store
+
+  validates :title, :content, presence: true
+  validates :user_id, :store_id, presence: true, on: :create
+  validates :title, uniqueness: true
+
+  def self.last_five_reviews
+    includes(:user, :store).last(5).reverse
+  end
 end
