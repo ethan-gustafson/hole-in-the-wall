dcd8903b5aa54f62bdd351f9aba32c8486dcb196
Ethan <ethan_gustafson@outlook.com>
2021-01-03 11:11:22 -0500

    change redirect method calls, index route instance variables, add user strong params in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   get "/users/new" do
-    redirect_current_user_back_to_root?
+    redirect_current_user_to_root?
     erb :'/users/new'
   end
 
   post "/users" do
-    @user = new_strong_params(params, User, :name, :username, :email, :password)
+    @user = User.new(**new_user_params)
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path 
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   end
 
   get "/users/accounts/:id" do
-    redirect_logged_out_user_to_login?
+    redirect_non_user_to_login?
     @current_page = params[:id].to_i
     listed_users_by_accounts_page(@current_page)
     if @current_page > @last_page || @current_page < 1
@@ -26,26 +26,29 @@ class UsersController < ApplicationController
   end 
 
   get "/users/:id" do
-    redirect_logged_out_user_to_login?
+    redirect_non_user_to_login?
     if is_current_user
-      @user            = current_user
-      @reviews         = current_user_reviews
-      @favorites       = current_user_favorites
-      @stores          = current_user_stores
-      @reviews_count   = Review.where(user_id: @user.id).count
-      @favorites_count = Favorite.where(user_id: @user.id).count
-      @stores_count    = Store.where(user_id: @user.id).count
+      @user = {
+        current: current_user,
+        reviews: current_user_reviews,
+        favorites: current_user_favorites,
+        stores: current_user_stores,
+        reviews_count: Review.where(user_id: current_user.id).count,
+        favorites_count: Favorite.where(user_id: current_user.id).count,
+        stores_count: Store.where(user_id: current_user.id).count
+      }
     else
-      @user               = User.find_by_id(params[:id])
-      @user_reviews_count = Review.where(user_id: @user.id).count
-      @reviews            = Review.limit(5).where(user_id: @user.id)
+      @user = {
+        current: User.find_by_id(params[:id]),
+        reviews_count: Review.where(user_id: params[:id]).count,
+        reviews: Review.limit(5).where(user_id: params[:id])
+      }
     end
     erb :'/users/show'
   end
 
   patch "/users/:id" do
-    if is_current_user && 
-       update_strong_params(params, User, current_user, :name, :username, :email)
+    if is_current_user && current_user.update(**update_user_params)
       redirect_to user_path(current_user.id)
     else
       flash[:invalid_update] = "Invalid Edit"
@@ -54,9 +57,19 @@ class UsersController < ApplicationController
   end
 
   get "/users/:id/delete" do
-    redirect_logged_out_user_to_login?
+    redirect_non_user_to_login?
     current_user.destroy
     session.clear
     redirect_to login_path
+  end
+
+  private
+
+  def new_user_params
+    attr_to_object(:user, :name, :username, :email, :password)
+  end
+
+  def update_user_params
+    attr_to_object(:user, :name, :username, :email)
   end
 end
