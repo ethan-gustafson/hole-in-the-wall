313e745c6ad07b703c7ac1d7039c07037f5a2eb5
Ethan <ethan_gustafson@outlook.com>
2021-01-03 11:09:52 -0500

    add private strong review params methods to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,20 +1,19 @@
 class ReviewsController < ApplicationController
   post "/reviews" do
-    @review = new_strong_params(params, Review, :title, :content, :user_id, :store_id)
+    @review = Review.new(**new_review_params)
     redirect_to store_path(@review.store_id) if @review.save || !@review.save
   end
 
   get "/reviews/:id/edit" do
-    redirect_logged_out_user_to_login?
-    set_review
+    redirect_non_user_to_login?
+    find_review
     @store = Store.find_by_id(@review.store_id)
     erb :'/reviews/edit'
   end
 
   patch "/reviews/:id" do
-    set_review
-    if review_belongs_to_user && 
-       update_strong_params(params, Review, @review, :title, :content)
+    find_review
+    if review_belongs_to_user && Review.update(**update_review_params)
       redirect_to store_path(@review.store_id)
     else
       redirect_to store_path(@review.store_id)
@@ -22,12 +21,22 @@ class ReviewsController < ApplicationController
   end
 
   delete "/reviews/:id" do 
-    set_review
+    find_review
     store_id = @review.store_id
     if review_belongs_to_user && @review.destroy 
       redirect_to store_path(store_id)
     else
       redirect_to store_path(store_id)
     end
+  end
+
+  private
+
+  def new_review_params
+    attr_to_object(:review, :title, :content, :user_id, :store_id)
+  end
+
+  def update_review_params
+    attr_to_object(:review, :title, :content)
   end
 end
